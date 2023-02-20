9834aba469fd6ed65dec43e9148509694d61d76d
Shann Marcus Ferancullo <ferancullo.shann@gmail.com>
2023-02-20 13:17:09 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@babel/eslint-parser',
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+    'react/state-in-constructor': 0,
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+// node.js inbuilt libarary
+// no need to install
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// Old Javascript Technique to Export content
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        // sequance should be right to left
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    compress: true,
+    port: 8080,
+    open: true,
+    historyApiFallback: true,
+  },
+};
